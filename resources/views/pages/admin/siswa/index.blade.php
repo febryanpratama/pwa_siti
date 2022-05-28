c63b6ee1ee0c200ec6d82c2e0b0d15d33975e911
febryanpratama <spandupan46@student.untan.ac.id>
2022-05-28 19:12:38 +0700

    Revisi Table siswa Admin

--- resources/views/pages/admin/siswa/index.blade.php
+++ resources/views/pages/admin/siswa/index.blade.php
@@ -33,6 +33,7 @@
                         <th>Telpon Siswa</th>
                         <th>Jenis Kelamin</th>
                         <th>Alamat</th>
+                        <th>Status Siswa</th>
                         <th>Nama Ortu</th>
                         <th>Telpon Ortu</th>
                         <th>Aksi</th>
@@ -46,11 +47,35 @@
                             <td>{{ $key->telpon_siswa }}</td>
                             <td>{{ $key->jenis_kelamin }}</td>
                             <td>{{ $key->alamat }}</td>
+                            <td>
+                                @switch($key->status)
+                                    @case('Gratis')
+                                        <div class="badge badge-warning">
+                                            {{ $key->status }}
+                                        </div>
+                                        @break
+                                        @case('Tidak Gratis')
+                                        <div class="badge badge-info">
+                                            {{ $key->status }}
+                                        </div>
+                                        
+                                        @break
+                                    @default
+                                        
+                                @endswitch
+                            </td>
                             <td>{{ $key->nama_ortu }}</td>
                             <td>{{ $key->telpon_ortu_siswa }}</td>
                             <td>
-                                <button class="btn btn-danger">
-                                    <a>Hapus</a>
+                                <button class="btn btn-sm btn-outline-info">
+                                    <svg xmlns="http://www.w3.org/2000/svg" style="width: 20px;height: 20px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                    </svg>
+                                </button>
+                                <button class="btn btn-sm btn-outline-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" style="width: 20px; height: 20px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
                                 </button>
                             </td>
                         </tr>
