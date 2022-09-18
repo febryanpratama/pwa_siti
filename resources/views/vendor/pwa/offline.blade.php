4eea10ff6cfdb637eb85db3be4719c0927357c1d
febryanpratama <spandupan46@student.untan.ac.id>
2022-09-18 14:27:55 +0700

    feat: rev offline

--- resources/views/vendor/pwa/offline.blade.php
+++ resources/views/vendor/pwa/offline.blade.php
@@ -2,5 +2,35 @@
 
 @section('content')
 	<!-- Put Offline content. -->
-    <h1>You are currently not connected to any networks. Maybe you are offline. Please check your internet connection.</h1>
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-wrapper-before"></div>
+            <div class="content-header row">
+                <div class="content-header-left col-md-4 col-12 mb-2">
+                    <h3 class="content-header-title">{{ $title }} DataTable</h3>
+                </div>
+                <div class="content-header-right col-md-8 col-12">
+                    <div class="breadcrumbs-top float-md-right">
+                        <div class="breadcrumb-wrapper mr-1">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item active">{{ $title }} DataTable
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <!-- Base style table -->
+                <section id="base-style">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1>You are currently not connected to any networks. Maybe you are offline. Please check your internet connection.</h1>
+                        </div>
+                    </div>
+                </section>
+                <!--/ Base style table -->
+            </div>
+        </div>
+    </div>
 @endsection
