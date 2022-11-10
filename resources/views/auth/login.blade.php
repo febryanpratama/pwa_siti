4eaa7d806d554e315bbd84edf746d820697df857
febryanpratama <spandupan46@student.untan.ac.id>
2022-11-10 09:46:00 +0700

    feat : add assets login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,30 +8,30 @@
       <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
       <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
       <meta name="author" content="pixelstrap">
-      <link rel="icon" href="https://alamandadevelopment.com/assets/images/favicon.png" type="image/x-icon">
-      <link rel="shortcut icon" href="https://alamandadevelopment.com/assets/images/favicon.png" type="image/x-icon">
+      <link rel="icon" href="{{ asset('') }}log/assets/images/favicon.png" type="image/x-icon">
+      <link rel="shortcut icon" href="{{ asset('') }}log/assets/images/favicon.png" type="image/x-icon">
       <title>SMA - TUNAS MULIA</title>
       <!-- Google font-->
       <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/font-awesome.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/font-awesome.css">
       <!-- ico-font-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/vendors/icofont.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/vendors/icofont.css">
       <!-- Themify icon-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/vendors/themify.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/vendors/themify.css">
       <!-- Flag icon-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/vendors/flag-icon.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/vendors/flag-icon.css">
       <!-- Feather icon-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/vendors/feather-icon.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/vendors/feather-icon.css">
       <!-- Plugins css start-->
       <!-- Plugins css Ends-->
       <!-- Bootstrap css-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/vendors/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/vendors/bootstrap.css">
       <!-- App css-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/style.css">
-      <link id="color" rel="stylesheet" href="https://alamandadevelopment.com/assets/css/color-1.css" media="screen">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/style.css">
+      <link id="color" rel="stylesheet" href="{{ asset('') }}log/assets/css/color-1.css" media="screen">
       <!-- Responsive css-->
-      <link rel="stylesheet" type="text/css" href="https://alamandadevelopment.com/assets/css/responsive.css">
+      <link rel="stylesheet" type="text/css" href="{{ asset('') }}log/assets/css/responsive.css">
       @PWA
    </head>
    <body>
@@ -91,19 +91,19 @@
             </div>
          </div>
          <!-- latest jquery-->
-         <script src="https://alamandadevelopment.com/assets/js/jquery-3.5.1.min.js"></script>
+         <script src="{{ asset('') }}log/assets/js/jquery-3.5.1.min.js"></script>
          <!-- Bootstrap js-->
-         <script src="https://alamandadevelopment.com/assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+         <script src="{{ asset('') }}log/assets/js/bootstrap/bootstrap.bundle.min.js"></script>
          <!-- feather icon js-->
-         <script src="https://alamandadevelopment.com/assets/js/icons/feather-icon/feather.min.js"></script>
-         <script src="https://alamandadevelopment.com/assets/js/icons/feather-icon/feather-icon.js"></script>
+         <script src="{{ asset('') }}log/assets/js/icons/feather-icon/feather.min.js"></script>
+         <script src="{{ asset('') }}log/assets/js/icons/feather-icon/feather-icon.js"></script>
          <!-- scrollbar js-->
          <!-- Sidebar jquery-->
-         <script src="https://alamandadevelopment.com/assets/js/config.js"></script>
+         <script src="{{ asset('') }}log/assets/js/config.js"></script>
          <!-- Plugins JS start-->
          <!-- Plugins JS Ends-->
          <!-- Theme js-->
-         <script src="https://alamandadevelopment.com/assets/js/script.js"></script>
+         <script src="{{ asset('') }}log/assets/js/script.js"></script>
          <!-- login js-->
          <!-- Plugin used-->
       </div>
