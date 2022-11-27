545b73d5b0eb615e1ef710ae7c691580e3df989b
febryanpratama <spandupan46@student.untan.ac.id>
2022-11-27 13:22:13 +0700

    feat: rev alumni

--- resources/views/pages/admin/alumni/index.blade.php
+++ resources/views/pages/admin/alumni/index.blade.php
@@ -29,10 +29,7 @@
                                     <h4 class="card-title">List Data</h4>
                                     <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                                     <div class="heading-elements">
-                                        {{-- <button type="button" class="btn btn-info " data-toggle="modal"
-                                            data-target="#large">
-                                            Add {{ $title }}
-                                        </button> --}}
+                                        
                                         <a href="{{ url('admin/alumni/get') }}">
                                             <button class="btn btn-info">Check Alumni</button>
                                         </a>
@@ -50,17 +47,13 @@
                                                         <th>Telpon Siswa</th>
                                                         <th>Jenis Kelamin</th>
                                                         <th>Alamat</th>
-                                                        {{-- <th>Status Siswa</th> --}}
-                                                        {{-- <th>Nama Ortu</th>
-                                                        <th>Telpon Ortu</th> --}}
                                                         <th>Kekurangan Pembayaran <br> Spp x Bulan</th>
                                                         <th>Status</th>
-
                                                         <th>Aksi</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @foreach ($alumni as $item=>$key)
+                                                   @foreach ($alumni as $item=>$key)
                                                         <tr>
                                                             <td>{{ $item+1 }}</td>
                                                             <td>{{ $key->nama_siswa }}</td>
@@ -75,8 +68,7 @@
                                                                     <span class='badge badge-success'>Lunas</span>
                                                                 @endif
                                                             </td>
-                                                            <td class="d-flex"> 
-
+                                                            <td> 
                                                                 @if (App\Helpers\Format::checkIjazah($key->id) == 0)
                                                                     <button class="btn btn-sm btn-outline-primary mr-1" data-toggle="modal" data-target="#tambahIjazah{{ $key->id }}" title="Tambah Ijazah">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" style="width: 20px;height: 20px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -84,52 +76,48 @@
                                                                         </svg>
                                                                     </button>
                                                                 @endif
-
-                                                                @if (App\Helpers\Format::checkSpp($key->id) >= 0)
+                                                                {{-- @if (App\Helpers\Format::checkSpp($key->id) >= 0)
                                                                     <button class="btn btn-sm btn-outline-info mr-1" data-toggle="modal" data-target="#pelunasan{{ $key->id }}" title="Pelunasan">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" style="width: 20px;height: 20px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                                                         </svg>
                                                                     </button>
-                                                                    <div class="modal fade text-left" id="pelunasan{{ $key->id }}" tabindex="-1"
-                                                            role="dialog" aria-labelledby="myModalLabel18"
-                                                            aria-hidden="true">
-                                                            <div class="modal-dialog" role="document">
-                                                                <div class="modal-content">
-                                                                    <div class="modal-header">
-                                                                        <h4 class="modal-title" id="myModalLabel17">
-                                                                            Add  Ijazah
-                                                                        </h4>
-                                                                        <button type="button" class="close"
-                                                                            data-dismiss="modal" aria-label="Close">
-                                                                            <span aria-hidden="true">&times;</span>
-                                                                        </button>
-                                                                    </div>
-                                                                    <div class="modal-body">
-                                                                        <form method="POST" action="{{ url("admin/alumni/pelunasan") }}" enctype="multipart/form-data">
-                                                                            <input type="hidden" value="{{ $key->id }}" name="siswa_id">
-                                                                            @csrf
-                                                                            <div class="row">
-                                                                                <div class="col-md-12">
-                                                                                    <div class="form-group">
-                                                                                        <label for="" class="control-label">Sisa Pembayaran SPP</label>
-                                                                                        <input type="text" name="nomor_ijazah" class="form-control" value="{{ number_format(App\Helpers\Format::getAllSisaSpp($key->id), 0) }}" placeholder="Masukkan Nomor Ijazah" readonly>
-                                                                                    </div>
+                                                                    <div class="modal fade text-left" id="pelunasan{{ $key->id }}" tabindex="-1"role="dialog" aria-labelledby="myModalLabel18"aria-hidden="true">
+                                                                        <div class="modal-dialog" role="document">
+                                                                            <div class="modal-content">
+                                                                                <div class="modal-header">
+                                                                                    <h4 class="modal-title" id="myModalLabel17">
+                                                                                        Add  Ijazah
+                                                                                    </h4>
+                                                                                    <button type="button" class="close"
+                                                                                        data-dismiss="modal" aria-label="Close">
+                                                                                        <span aria-hidden="true">&times;</span>
+                                                                                    </button>
+                                                                                </div>
+                                                                                <div class="modal-body">
+                                                                                    <form method="POST" action="{{ url("admin/alumni/pelunasan") }}" enctype="multipart/form-data">
+                                                                                        <input type="hidden" value="{{ $key->id }}" name="siswa_id">
+                                                                                        @csrf
+                                                                                        <div class="row">
+                                                                                            <div class="col-md-12">
+                                                                                                <div class="form-group">
+                                                                                                    <label for="" class="control-label">Sisa Pembayaran SPP</label>
+                                                                                                    <input type="text" name="nomor_ijazah" class="form-control" value="{{ number_format(App\Helpers\Format::getAllSisaSpp($key->id), 0) }}" placeholder="Masukkan Nomor Ijazah" readonly>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                        <div class="form-group row">
+                                                                                            <label class="col-sm-2 col-form-label"></label>
+                                                                                            <div class="col-sm-10">
+                                                                                                <button type="submit" class="btn btn-primary  px-5 float-right "><i class="icon"></i>+ Pelunasan</button>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </form>
                                                                                 </div>
                                                                             </div>
-                                                                            <div class="form-group row">
-                                                                                <label class="col-sm-2 col-form-label"></label>
-                                                                                <div class="col-sm-10">
-                                                                                    <button type="submit" class="btn btn-primary  px-5 float-right "><i class="icon"></i>+ Pelunasan</button>
-                                                                                </div>
-                                                                            </div>
-                                                                        </form>
+                                                                        </div>
                                                                     </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                                @endif
-
+                                                                @endif --}}
                                                             </td>
                                                         </tr>
                                                         <div class="modal fade text-left" id="tambahIjazah{{ $key->id }}" tabindex="-1"
@@ -193,7 +181,6 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        
                                                     @endforeach
                                                 </tbody>
                                                 <tfoot>
@@ -216,6 +203,7 @@
                         </div>
                     </div>
                 </section>
+                
                 <!--/ Base style table -->
             </div>
         </div>
