224f8160a72c281e2259b5575a809c1bbba034a7
febryanpratama <spandupan46@student.untan.ac.id>
2023-01-03 21:01:14 +0700

    feat: rev laporan

--- app/Services/LaporanSppService.php
+++ app/Services/LaporanSppService.php
@@ -33,7 +33,7 @@ class LaporanSppService
     static function exportExcel($data)
     {
 
-        $data = DetailKelas::with('kelas', 'siswa')->where('kelas_id', $data['kelas_id'])->get();
+        $data = DetailKelas::with('kelas', 'siswa')->where('kelas_id', $data['kelas_id'])->OrderBy('siswa.nama_siswa', 'ASC')->get();
 
         // dd($data);
 
