467a49ff7f7c1c7636e326252fbec69a4a0b69e3
febryanpratama <spandupan46@student.untan.ac.id>
2023-02-21 00:06:34 +0700

    rev pwa

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use App\Http\Controllers\GuruController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\KepsekController;
 use App\Http\Controllers\LaporanSppController;
-use CodexShaper\PWA\Facades\PWA;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -200,4 +199,4 @@ Route::group(['prefix' => 'user', 'middleware' => ['role:User']], function () {
 
 Route::post('admin/alumni/pelunasan', [AlumniController::class, 'pelunasan'])->name('alumni.pelunasan');
 
-PWA::routes();
+\PWA::routes();
