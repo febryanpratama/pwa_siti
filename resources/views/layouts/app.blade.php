7aa8f0d56b2210e688f07f6eb832718759120e14
febryanpratama <spandupan46@student.untan.ac.id>
2023-02-21 00:51:38 +0700

    rev data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,14 @@
 <!-- BEGIN: Head-->
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    {{-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description"
         content="Chameleon Admin is a modern Bootstrap 4 webapp &amp; admin dashboard html template with a large number of components, elegant design, clean and organized code.">
     <meta name="keywords"
         content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
-    <meta name="author" content="ThemeSelect">
+    <meta name="author" content="ThemeSelect"> --}}
     @hasSection('title')
         <title>@yield('title') - {{ config('app.name') }}</title>
     @else
