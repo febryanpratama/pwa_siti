ba1a8223a4dc7129d1c46a5c22651b22da956815
febryanpratama <spandupan46@student.untan.ac.id>
2023-02-21 00:10:34 +0700

    feat: data app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // Schema::defaultStringLength(191);
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
     }
 }
