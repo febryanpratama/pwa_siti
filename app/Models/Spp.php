3f8666de57ab312b34d369734ffb068d7e464368
febryanpratama <spandupan46@student.untan.ac.id>
2023-02-21 23:39:59 +0700

    feat: rev pwa

--- app/Models/Spp.php
+++ app/Models/Spp.php
@@ -13,7 +13,7 @@ class Spp extends Model
 
     public function siswa()
     {
-        return $this->belongsTo(siswa::class, 'siswa_id');
+        return $this->belongsTo(siswa::class, 'siswa_id')->withTrashed();
     }
 
     public function guru()
